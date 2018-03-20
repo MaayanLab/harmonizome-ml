a859c7bb2145dd07faa76f485792766e76188653
Daniel J. B. Clarke <u8sand@gmail.com>
2018-03-20 13:05:57 -0400

    Updated prefix to not use leading slash

--- app/util.py
+++ app/util.py
@@ -2,7 +2,7 @@ import os
 
 app_dir = os.path.dirname(os.path.realpath(__file__))
 data_dir = os.path.dirname(os.path.realpath(__file__)) + '/../data'
-PREFIX = os.environ.get('HARMONIZOME_API_PREFIX', '')
+PREFIX = '/' + os.environ.get('HARMONIZOME_API_PREFIX', '')
 MYSQL_HOST = os.environ.get('MYSQL_HOST', '')
 MYSQL_DATABASE = os.environ.get('MYSQL_DATABASE', '')
 MYSQL_USER = os.environ.get('MYSQL_USER', '')
